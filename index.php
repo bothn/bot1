571d1626b781e084a65387c4587ba62f00681071
bothn <92487044+bothn@users.noreply.github.com>
2021-10-13 20:18:20 -0600

    Update index.php

--- index.php
+++ index.php
@@ -62,8 +62,8 @@ $weather = json_decode(file_get_contents ("http://190.4.63.192/reportes/bot/supe
     foreach ($weather as $pc) { 
   $x= $pc["NPA"];
   if ($x >= 99 ){$emoj2 = "\xF0\x9F\x98\x83";} 
- if ( $x > 96 && $x < 99 ){$emoj2 ="";}   
- if ( $x <= 96 ){$emoj2 ="\xF0\x9F\x90\xA2";}
+ if ( $x > 97 && $x < 99 ){$emoj2 ="";}   
+ if ( $x <= 97 ){$emoj2 ="\xF0\x9F\x90\xA2";}
   
     $msg .= urlencode("<pre> ".$pc["NOMBRE"]." => ".$pc["NPA"]."</pre>")." ".$emoj2.urlencode("\n");
  
